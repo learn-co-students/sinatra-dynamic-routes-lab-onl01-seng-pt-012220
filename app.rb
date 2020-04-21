f783d77b9a4fa2c8722be9ab86fd83c382b275f9
LeonorPDX <leonoracolbert@gmail.com>
2020-04-21 13:01:01 -0700

    Done.

--- app.rb
+++ app.rb
@@ -2,18 +2,17 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reversename/:name' do
-      "#{params[:name].reverse}"
+      params[:name].reverse
   end
 
   get '/square/:number' do
-    @square = params[:number].to_i * params[:number].to_i
-    "#{@square}"
+    (params[:number].to_i ** 2).to_s
   end
 
   get '/say/:number/:phrase' do
     @arr = []
     params[:number].to_i.times {@arr << params[:phrase]}
-    "#{@arr.join(" ")}"
+    @arr.join(" ")
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
@@ -36,7 +35,7 @@ class App < Sinatra::Base
       @solution = @n1 / @n2
     end
 
-    "#{@solution}"
+    @solution.to_s
 
   end
 end
